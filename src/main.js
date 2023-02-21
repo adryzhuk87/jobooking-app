be53c17cedc0824bb78db5c120c101f234dbad02
qwerty1987 <adryzhuk@gmail.com>
2023-02-21 16:37:01 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
 
-import App from "./App.vue";
-import router from "./router";
+// Components
+import App from './App.vue'
 
-import "./assets/main.css";
+// Composables
+import { createApp } from 'vue'
 
-const app = createApp(App);
+// Plugins
+import { registerPlugins } from '@/plugins'
 
-app.use(createPinia());
-app.use(router);
+const app = createApp(App)
 
-app.mount("#app");
+registerPlugins(app)
+
+app.mount('#app')
